4cedf00e35e0b3978c1e82d5a784c5016a91c5ae
K.RAVITEJA <kummariraviteja2003@gmail.com>
2023-06-21 21:24:21 +0530

    problems

---
+++ Remove_duplicates_Array.java
@@ -0,0 +1,15 @@
+//User function Template for Java
+//two pointer approach
+class Solution {
+    int remove_duplicate(int A[],int N){
+        // code here
+        int i=0;
+        for(int j=1;j<N;j++){
+            if(A[j]!=A[i]){
+                A[i+1]=A[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}
