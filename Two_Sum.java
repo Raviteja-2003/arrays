929e6019f014c8cc74733ab431b5e18b558b3fa5
K.RAVITEJA <kummariraviteja2003@gmail.com>
2023-06-26 00:36:07 +0530

    day-4

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       ArrayList<Integer> ar=new ArrayList<>();
        HashMap<Integer,Integer> map=new HashMap<>();
        for(int i=0;i<nums.length;i++){
            if(map.containsKey(target-nums[i])){
