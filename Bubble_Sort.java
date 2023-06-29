eb695a08998eb9b670bceac458a158cb3de601d0
K.RAVITEJA <kummariraviteja2003@gmail.com>
2023-06-29 21:05:46 +0530

    Day8

---
+++ Bubble_Sort.java
@@ -0,0 +1,23 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        
+   
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+        
+    }
+        
+    }
+}
